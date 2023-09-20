4b5192078499cb9cf48fbca7183cd9858272040c
Enzo <enzo.gallone83@gmail.com>
2023-09-20 15:28:01 -0300

    pagina add produtos feitas

--- ex2/src/App.jsx
+++ ex2/src/App.jsx
@@ -6,28 +6,13 @@ import Rodape from "./components/Rodape";
 
 export default function App() {
 
-  // const [produto, setProduto] = useState("PRODUTO");
-
-  // let count = 0;
-  // const novoValor =()=>{
-  //   count++;
-  //   setProduto(produto +" "+ count);
-  // }
-
   return (
+    
     <>
       <div>
-        {/* Isso é um comentário */}
-
-        {/* Vamos criar uma cabeçalho com um header, um h1 com um título e uma lista ul com 3 item com link. */}
-        <Cabecalho />
-        {/* Vamos criar uma seção com uma div, com 3 <p></p> e estes <p></p> vão ter 3 linhas de lorem, abaixo dos <p></p> uma img. */}
-            {/* <p>Valor do useState :{produto}</p>
-            <button onClick={novoValor}>CLICK</button> */}
-            <Outlet/>
-
-        <Rodape />
-        {/* Vamos criar um rodapé com uma div, uma lista com 3 itens com links para redes sociais e abaixo um <p></p> com o texto e código do símbolo de direitos reservados. */}
+      <Cabecalho />
+      <Outlet/>
+      <Rodape />
       </div>
     </>
   );
