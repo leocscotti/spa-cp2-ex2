9133e64735b8ca3eb2e13e558a88bf208388743b
leocscotti <leoscotti03@gmail.com>
2023-09-20 15:23:02 -0300

    pagina main ajustada

--- ex2/src/main.jsx
+++ ex2/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './routes/Home.jsx';
+import EditarProdutos from './routes/EditarProdutos.jsx';
+import Erro404 from './routes/Erro404.jsx';
+import Produtos from './routes/Produtos.jsx';
+import ExcluirProduto from './routes/ExcluirProduto.jsx';
+
+const router = createBrowserRouter([
+  {path: '/', element: <App/>,
+    errorElement: <Erro404/>,
+    children:[
+      {path: '/', element: <Home/>},
+      {path: '/produtos', element: <Produtos/>},
+      {path: '/editar/produtos/:id', element: <EditarProdutos/>},
+      {path: '/excluir/produtos/:id', element: <ExcluirProduto/>},
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+       <RouterProvider router={router}/>
+  ,
 )
